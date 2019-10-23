06295af15b7dd30506d61c3d2c070b99b1055f80
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-10-23 09:40:26 +0200

    commit

--- eindkapitaal/Program.cs
+++ eindkapitaal/Program.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿//Enrick De Munter 5ITN nr4 23/10/19
+/*analyse 
+ * vraag het beginkapitaal
+ * vraag de looptijd
+ * 
+ * bereken intrestvoet(/100*1.25)
+ * bereken beginkapitaalinput
+ * looptijdinput
+ * bereken eindkapitaal(bkinput * (1+(/100*1.25)looptijdinput
+ * 
+ * toon eindkapitaal in 2 cijfers na de komma
+ * 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +24,39 @@ namespace eindkapitaal
     class Program
     {
         static void Main(string[] args)
-        {
+        {//Declaratie 
+            string beginkapitaalinput, looptijdinput;
+            decimal eindkapitaal;
+            string intrestvoet, looptijd;
+            decimal beginkapitaal;
+
+
+
+            //input
+            //vraag het beginkapitaal
+            Console.WriteLine("wat is het beginkapitaal");
+            beginkapitaalinput = Console.ReadLine();
+            //vraag de looptijd
+            Console.WriteLine("wat is de looptijd: ");
+            looptijdinput = Console.ReadLine();
+
+            //bereken intrestvoet(/100*1.25)
+            intrestvoet = "1.25 %";
+            //bereken beginkapitaalinput
+            beginkapitaal = decimal.Parse(beginkapitaalinput);
+            //looptijdinput
+            looptijd = looptijdinput;
+            //bereken eindkapitaal(bkinput* (1+(/100*1.25)looptijdinput
+
+
+            //toon eindkapitaal in 2 cijfers na de komma
+
+            //wachten
+            Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten: ");
+            Console.ReadKey();
         }
+        
+
     }
 }
